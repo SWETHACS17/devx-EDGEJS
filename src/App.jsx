e67546d6edc332b6786cc8af0299824d5fe0bad2
Raam Prathap R V <144192481+Raamprathap@users.noreply.github.com>
2025-03-18 15:48:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Profilelog from "./Auth/Profilelog";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/profilelog" element={<Profilelog />} />
       </Routes>
     </Router>
   );
